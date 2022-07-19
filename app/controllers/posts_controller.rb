ef88a441129ec021c1fb76902f2ae731fd4649f6
Hunter Kassow <hunterkassow@protonmail.com>
2022-07-19 17:12:51 -0400

    commit tree 91b908aa5463c9639cd94b488357e8fb04664e2a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordInvalid, with: :render_bad
   def show
     post = Post.find(params[:id])
     
@@ -9,12 +9,16 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
+    
+    post.update!(post_params)
 
     render json: post
   end
 
   private
+  def render_bad(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 
   def post_params
     params.permit(:category, :content, :title)
